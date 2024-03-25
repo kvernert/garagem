419faf18f402e073ae564782ad5cd0cb21e20af3
kvernert <kverner47@gmail.com>
2024-03-25 16:52:36 -0300

    Criação da API Marca

--- app/urls.py
+++ app/urls.py
@@ -14,13 +14,14 @@ from core.views import UserViewSet
 from core.views import AcessorioViewSet # nova linha
 from core.views import CategoriaViewSet
 from core.views import CorViewSet # nova linha
+from core.views import MarcaViewSet
 
 router = DefaultRouter()
 router.register(r"acessorios", AcessorioViewSet) # nova linha
 router.register(r"categorias", CategoriaViewSet) # nova linha
 router.register(r"cores", CorViewSet) # nova linha
 router.register(r"users", UserViewSet, basename="users")
-...
+router.register(r"marcas", MarcaViewSet,) 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
--- core/models/__init__.py
+++ core/models/__init__.py
@@ -3,3 +3,4 @@ from .user import User
 from .acessorio import Acessorio
 from .categoria import Categoria
 from .cor import Cor
+from .marca import Marca
